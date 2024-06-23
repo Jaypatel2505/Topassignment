721cf41cb4323212b10ac57df8d99df9f206d24f
Jaypate2505 <joy.patel2505@gmail.com>
2024-06-23 22:06:53 +0530

    Some added

---
+++ src/com/assignment/GreatestNumber.java
@@ -0,0 +1,10 @@
+package com.assignment;
+/*
+ * W.A.J.P to Take three numbers from the user and print the greatest number.
+ */
+public class GreatestNumber {
+	public static void main(String[] args) {
+		
+	}
+
+}
