95f81985c36924c49878b8a413f344f2e1f4d44a
Jaypate2505 <joy.patel2505@gmail.com>
2024-06-23 16:28:45 +0530

    Rename file and content change

--- src/com/assignment/FirstPattern.java
+++ src/com/assignment/FirstPattern.java
@@ -1,6 +1,6 @@
 package com.assignment;
 
-public class PatternAssignment {
+public class FirstPattern {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= i; j++) {
